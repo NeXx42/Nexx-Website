794666cc02c175b7fdd88c64bc5a729d89807318
Matthew <matthew.g.e123@gmail.com>
2021-06-20 12:00:34 +0100

    Fixed Theme saving

--- Javascript.js
+++ Javascript.js
@@ -1,39 +1,31 @@
 let curTheme = 1; 
 
 function ChangeTheme(){
+  curTheme = curTheme == 0 ? 1 : 0;
+  
+  if(curTheme == 1){
+    document.body.classList = 'dark';
+  }
+  else{
+    document.body.classList = 'light';
+  }
     
-    if(curTheme == 1){
-
-        document.body.classList.replace('light', 'dark');
-    }
-    else{
-
-        document.body.classList.replace('dark', 'light');
-    }
-    
-    curTheme = curTheme == 0 ? 1 : 0;
-    document.cookie = `theme=${curTheme};`;
+  localStorage.setItem("theme", curTheme);
 }
 
 
 function LoadTheme(){
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
+  try{
+    curTheme = localStorage.theme;
+  }
+  catch{
+    curTheme = 1;
+  }
 
-    for(var i = 0; i <ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-
-      if (c.indexOf("theme") == 0) {
-        curTheme = parseInt(c.substring("theme".length, c.length));
-      }
-    }
-
-
-    if(curTheme == 1)
-        document.body.classList.replace('light', 'dark');
-    else
-        document.body.classList.replace('dark', 'light');
+  if(curTheme == 1){
+    document.body.classList = 'dark';
+  }
+  else{
+    document.body.classList = 'light';
+  }
 }
